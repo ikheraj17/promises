f14bd4d6d72ba85e65d7105e5250341adfc16d00
Adam Tiller <a.j.tiller@gmail.com>
2020-04-14 10:12:45 -0700

    Add getResponseCode to callbackReview.

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -22,7 +22,13 @@ var pluckFirstLineFromFile = function (filePath, callback) {
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
-  // TODO
+  request.get({url: url}, (err, res, bod) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
