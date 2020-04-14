74892b90c54a5cbea15f5660a4b0ffb1f6df291e
Adam Tiller <a.j.tiller@gmail.com>
2020-04-14 10:02:27 -0700

    Add readFirstLine to callbackReview.

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,14 +4,24 @@
 
 var fs = require('fs');
 var request = require('request');
+var readline = require('readline');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      let filetext = data.toString();
+      let endLine = filetext.indexOf('\n');
+      let firstLine = filetext.substring(0, endLine);
+      callback(null, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
 };
 
